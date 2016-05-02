4fa978c339dd4aab595d5bed3b5f1e9766567360
ryancheunggit <ryanzjlib@gmail.com>
2016-05-01 23:11:55 -0400

    tts

--- Casca/app/src/main/java/edu/bentley/casca/displayDetail.java
+++ Casca/app/src/main/java/edu/bentley/casca/displayDetail.java
@@ -1,14 +1,21 @@
 package edu.bentley.casca;
 import android.content.Intent;
 import android.net.Uri;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View.OnClickListener;
+import java.text.DateFormatSymbols;
 
-public class displayDetail extends AppCompatActivity {
+import java.util.Locale;
+
+public class displayDetail extends AppCompatActivity implements OnClickListener,OnInitListener {
 
     private SQLHelper helper;
     private TextView eventTitle;
@@ -17,6 +24,7 @@ public class displayDetail extends AppCompatActivity {
     private TextView eventEndTime;
     private TextView eventDate;
     private TextView eventDescription;
+    private TextToSpeech speaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,29 @@ public class displayDetail extends AppCompatActivity {
         eventEndTime.setText(ent.getEndTime());
         eventDate.setText(ent.getDateT());
         eventDescription.setText(ent.getDescription());
+        //add listener on eventTitle and eventDescription to speak out content
+        eventTitle.setOnClickListener(this);
+        eventDescription.setOnClickListener(this);
+
+        speaker = new TextToSpeech(this, this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.eventTitleDetail:
+                String returnDate = translateDate(eventDate.getText().toString());
+                String returnTime = translateTime(eventStartTime.getText().toString());
+                String speakEvent = eventTitle.getText().toString() +
+                        " The start time is " + returnTime + "Start Date is " + returnDate;
+                speak(speakEvent);
+                break;
+            case R.id.eventDescriptionDetail:
+                String speakEventDes = eventDescription.getText().toString();
+                speak(speakEventDes);
+                break;
+        }
+
     }
 
     @Override
@@ -81,6 +112,70 @@ public class displayDetail extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    public void speak(String output) {
+        speaker.speak(output, TextToSpeech.QUEUE_FLUSH, null, "Id 0");
+    }
+
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = speaker.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TextToSpeech", "Language is not available.");
+            } else {
+                Log.i("TextToSpeech", "TTS Initialization successful.");
+            }
+        } else {
+            Log.e("TextToSpeech", "Could not initialize TextToSpeech.");
+        }
+    }
+//change numerical event date to standard date format: 5-1-2015: May first 2015"
+    public String translateDate(String date) {
+        DateFormatSymbols symbols = new DateFormatSymbols();
+        String Year = date.substring(date.lastIndexOf("-") + 1);
+        String Month = date.substring(date.indexOf("-") + 1, date.lastIndexOf("-"));
+        String Day = date.substring(0, date.indexOf("-"));
+
+        Month = symbols.getMonths()[Integer.parseInt(Month) - 1];
+
+        String daySuffix = getDayOfMonthSuffix(Integer.parseInt(Day));
+        String ordinalToday = Day + daySuffix;
+        String returnDate = " "+ Month +" "+ ordinalToday +" " + Year;
+
+        return returnDate;
+    }
+//get correct suffix of dayofmonth
+    String getDayOfMonthSuffix(final int n) {
+        if (n >= 11 && n <= 13) {
+            return "th";
+        }
+        switch (n % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+//change time to standard time format for textToSpeach to function well
+    String translateTime(String time) {
+        String Hour = time.substring(0, time.indexOf(":"));
+        String Minute = time.substring(time.indexOf(":") + 1);
+        int h = Integer.parseInt(Hour);
+        int m = Integer.parseInt(Minute);
+        String returnTime = "";
+        if (h > 12) {
+            returnTime = (h - 12) + " " + Minute + " PM ";
+        } else if (h <= 12) {
+            returnTime = h + " " + Minute + " AM ";
+        }
+        return returnTime;
     }
 
 }
